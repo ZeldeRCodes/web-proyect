2eff2f509825094c5e6fb637ce7a19f0de5b37b5
ZeldeRCodes <zelder3096@gmail.com>
2026-01-19 11:30:43 -0500

    Delete app directory

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.auth.routes import router as auth_router
-from app.categories.routes import router as categories_router
-
-app = FastAPI()
-
-app.include_router(auth_router)
-app.include_router(categories_router)
